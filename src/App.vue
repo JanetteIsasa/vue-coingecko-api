13ddad662e5823fe7744eb72bee17cc0471ea0b7
patricia_valdez <pativaldez0@gmail.com>
2022-08-08 17:47:00 -0400

    I filled the table with the data

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,74 @@
 <template>
   <h1>hello</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>coin</th>
-      </tr>
-    </thead>
-    <tbody v-for="coin in coins" :key="coin.id">
-      <tr>
-        {{coin.name}}
-      </tr>
-    </tbody>
-  </table>
+  <div class="container">
+    <div class="row">
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th v-for="title in titles" :key="title">
+            {{title}}
+            </th>
+          </tr>
+        </thead>
+        <tbody v-for="(coin, index ) in coins" :key="coin.id">
+          <tr>
+            <td class="text-muted">
+              {{index + 1}}
+            </td>
+            <td>
+              <img :src="coin.image" style="width: 2rem;" class="me-2"/>
+              <span>
+                {{ coin.name }}
+              </span>
+              <span class="ms-2 text-uppercase text-muted">
+                {{coin.symbol}}
+              </span>
+            </td>
+            <td>
+              ${{coin.current_price}}
+            </td>
+            <td>
+              {{coin.price_change_percentage_24h}}
+            </td>
+            <td>
+              {{coin.total_volume}}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   //data es un metodo que retorna un objeto
-  data(){
-    return{
-      coins:[],
-    }
+  data() {
+    return {
+      coins: [], //arreglo donde van a ir los datos de la api
+      titles: [
+        '#',
+        'Coin',
+        'Price',
+        'Price Change',
+        '24h Volume',
+      ]
+    };
   },
   //cuando cargue la app va a hacer esta petición fetch a la api
-  async mounted(){
-    const resp = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+  async mounted() {
+    const resp = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
     const data = await resp.json(); //convierte en formato json
-    this.coins = data;
-  }
-}
+    this.coins = data; //pasa los datos al objeto coins
+  },
+};
 </script>
 
 <style>
-
+/* tr td img-coin{
+  width: 2 rem;
+} */
 </style>
