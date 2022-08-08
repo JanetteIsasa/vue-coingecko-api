80479c4dd4487c2f305d68e25f6ceb853d93e8e3
patricia_valdez <pativaldez0@gmail.com>
2022-08-08 18:58:29 -0400

    Logic and design finished

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,49 @@
 <template>
-  <h1>hello</h1>
   <div class="container">
     <div class="row">
+      <h1 class="pt-4">Coin Market</h1>
+       <input
+        type="text"
+        class="form-control text-light bg-dark rounded-0 border-0 my-4"
+        placeholder="Search"
+        v-model="textSearch"
+        @keyup="searchCoin()"
+        autofocus
+      />
+
       <table class="table table-dark">
         <thead>
           <tr>
             <th v-for="title in titles" :key="title">
-            {{title}}
+              {{ title }}
             </th>
           </tr>
         </thead>
-        <tbody v-for="(coin, index ) in coins" :key="coin.id">
+        <tbody v-for="(coin, index) in filteredCoins" :key="coin.id">
           <tr>
             <td class="text-muted">
-              {{index + 1}}
+              {{ index + 1 }}
             </td>
             <td>
-              <img :src="coin.image" style="width: 2rem;" class="me-2"/>
+              <img :src="coin.image" style="width: 2rem" class="me-2" />
               <span>
                 {{ coin.name }}
               </span>
               <span class="ms-2 text-uppercase text-muted">
-                {{coin.symbol}}
+                {{ coin.symbol }}
               </span>
             </td>
-            <td>
-              ${{coin.current_price}}
+            <td>${{ coin.current_price }}</td>
+            <td
+              :class="[
+                coin.price_change_percentage_24h > 0
+                  ? 'text-success'
+                  : 'text-danger',
+              ]"
+            >
+              {{ coin.price_change_percentage_24h }} %
             </td>
-            <td>
-              {{coin.price_change_percentage_24h}}
-            </td>
-            <td>
-              {{coin.total_volume}}
-            </td>
+            <td>$ {{ coin.total_volume.toLocaleString() }}</td>
           </tr>
         </tbody>
       </table>
@@ -47,13 +58,9 @@ export default {
   data() {
     return {
       coins: [], //arreglo donde van a ir los datos de la api
-      titles: [
-        '#',
-        'Coin',
-        'Price',
-        'Price Change',
-        '24h Volume',
-      ]
+      filteredCoins: [],
+      titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
+      textSearch: "",
     };
   },
   //cuando cargue la app va a hacer esta petición fetch a la api
@@ -63,12 +70,19 @@ export default {
     );
     const data = await resp.json(); //convierte en formato json
     this.coins = data; //pasa los datos al objeto coins
+     this.filteredCoins = data;
+  },
+  //evento creado para filtrar a la par que el usuario tipea
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+          coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
   },
 };
 </script>
 
-<style>
-/* tr td img-coin{
-  width: 2 rem;
-} */
-</style>
+
