200381dbb9688222ec4898c5e22d4457b010e29a
patricia_valdez <pativaldez0@gmail.com>
2022-08-09 20:57:20 -0400

    I consumed api with axios

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <h1 class="pt-4">Coin Market</h1>
-       <input
+      <input
         type="text"
         class="form-control text-light bg-dark rounded-0 border-0 my-4"
         placeholder="Search"
@@ -11,47 +11,60 @@
         autofocus
       />
 
-      <table class="table table-dark">
-        <thead>
-          <tr>
-            <th v-for="title in titles" :key="title">
-              {{ title }}
-            </th>
-          </tr>
-        </thead>
-        <tbody v-for="(coin, index) in filteredCoins" :key="coin.id">
-          <tr>
-            <td class="text-muted">
-              {{ index + 1 }}
-            </td>
-            <td>
-              <img :src="coin.image" style="width: 2rem" class="me-2" />
-              <span>
-                {{ coin.name }}
-              </span>
-              <span class="ms-2 text-uppercase text-muted">
-                {{ coin.symbol }}
-              </span>
-            </td>
-            <td>${{ coin.current_price }}</td>
-            <td
-              :class="[
-                coin.price_change_percentage_24h > 0
-                  ? 'text-success'
-                  : 'text-danger',
-              ]"
-            >
-              {{ coin.price_change_percentage_24h }} %
-            </td>
-            <td>$ {{ coin.total_volume.toLocaleString() }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <section v-if="errored">
+        <h4 class="text-center">
+          Lo sentimos, no es posible obtener la información en este momento, por
+          favor intente nuevamente mas tarde
+        </h4>
+      </section>
+
+      <section v-else>
+        <div v-if="loading">Cargando...</div>
+
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th v-for="title in titles" :key="title">
+                {{ title }}
+              </th>
+            </tr>
+          </thead>
+          <tbody v-for="(coin, index) in filteredCoins" :key="coin.id">
+            <tr>
+              <td class="text-muted">
+                {{ index + 1 }}
+              </td>
+              <td>
+                <img :src="coin.image" style="width: 2rem" class="me-2" />
+                <span>
+                  {{ coin.name }}
+                </span>
+                <span class="ms-2 text-uppercase text-muted">
+                  {{ coin.symbol }}
+                </span>
+              </td>
+              <td>${{ coin.current_price }}</td>
+              <td
+                :class="[
+                  coin.price_change_percentage_24h > 0
+                    ? 'text-success'
+                    : 'text-danger',
+                ]"
+              >
+                {{ coin.price_change_percentage_24h }} %
+              </td>
+              <td>$ {{ coin.total_volume.toLocaleString() }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
   //data es un metodo que retorna un objeto
@@ -59,18 +72,27 @@ export default {
     return {
       coins: [], //arreglo donde van a ir los datos de la api
       filteredCoins: [],
+      loading: true,
+      errored: false,
       titles: ["#", "Coin", "Price", "Price Change", "24h Volume"],
       textSearch: "",
     };
   },
-  //cuando cargue la app va a hacer esta petición fetch a la api
+  // en axios los datos estan en data, es por eso que el resultado se obtiene así
   async mounted() {
-    const resp = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
-    const data = await resp.json(); //convierte en formato json
-    this.coins = data; //pasa los datos al objeto coins
-     this.filteredCoins = data;
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        this.coins = response.data;
+        this.filteredCoins = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   },
   //evento creado para filtrar a la par que el usuario tipea
   methods: {
